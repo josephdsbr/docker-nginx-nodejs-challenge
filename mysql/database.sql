c21ea2e2120820ee6e81829c73a463fbeed69088
josephdsbr <josephdsbr@gmail.com>
2021-03-04 11:59:09 -0300

    ajustando conf do nginx e query do sql

--- mysql/database.sql
+++ mysql/database.sql
@@ -1,2 +1,2 @@
 USE nodedb;
-CREATE TABLE people (id int not null auto_increment, name varchar(255), primary key (id));
+CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name varchar(255), primary key (id));
